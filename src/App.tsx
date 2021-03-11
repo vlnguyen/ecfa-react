b6c8f4369b38afa1403700662346a135f96bec63
vlnguyen <VincentLocNguyen@gmail.com>
2021-03-11 01:19:06 -0500

    output ECFA2021.wf contents to console

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import Dropzone from 'react-dropzone';
 import logo from './img/ecfa2021-logo.png';
 import './App.css';
 
+function handleFileSelected<T extends File>(acceptedFiles: T[]) {
+  const fileReader = new FileReader();
+  fileReader.onloadend = async (e) => {
+    const content = fileReader.result;
+    console.log(content);
+  };
+  fileReader.readAsText(acceptedFiles[0]);
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)} multiple={false}>
+        <Dropzone onDrop={handleFileSelected} multiple={false}>
           {({ getRootProps, getInputProps }) => (
             <section>
               <div {...getRootProps()} className="Dropzone">
