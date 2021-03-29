da37a6ffb6c79eb30185ebc1fcecaf1d2842ca36
vlnguyen <VincentLocNguyen@gmail.com>
2021-03-28 22:28:04 -0400

    update for DLC #3

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,7 @@ function App() {
             </section>
           )}
         </Dropzone>
-        <p>Songlist last updated: March 27, 2021</p>
-        <p><i>Step count is fixed for Turndup.</i></p>
+        <p>Songlist last updated: March 28, 2021</p>
       </header>
     </div>
   );
